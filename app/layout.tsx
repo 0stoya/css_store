0e38450b64007f6edc57fec286ac25ffdd07d962
0stoya <chris@ostoya.co.uk>
2026-09-10 23:23:37 +0100

    feat: load delivery checkout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./pdp.css";
 import "./pdp-simplified.css";
 import "./pdp-runtime-tuning.css";
 import "./basket-ux.css";
+import "./delivery-ux.css";
 import { SiteFooter } from "@/components/site-footer";
 import { getStoreName } from "@/lib/config";
 
