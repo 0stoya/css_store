712e8b416d474e85dc46f6c84e2841fe55f0eb11
0stoya <chris@ostoya.co.uk>
2026-09-10 22:03:15 +0100

    style: load lucide icon system

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "./accessibility.css";
 import "./portal-ux.css";
 import "./production-fixes.css";
+import "./icon-system.css";
 import { SiteFooter } from "@/components/site-footer";
 import { getStoreName } from "@/lib/config";
 
